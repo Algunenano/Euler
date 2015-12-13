4452a7161774adb5392405ee8e55cb7de5e895f3
Algunenano <algunenano@gmail.com>
2015-12-13 18:52:19 +0100

    Problem 4 initial project

---
+++ euler4/include/Euler4.h
@@ -0,0 +1,5 @@
+#ifndef EULER4_H
+#define EULER4_H
+
+
+#endif
---
+++ euler4/test/Euler4Spec.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "Euler4.h"
+
+class Euler4Spec : public testing::Test
+{
+protected:
+
+    void SetUp()
+    {
+        
+    }
+
+    void TearDown()
+    {
+        // Teardown ...
+    }
+
+};
+
+TEST_F(Euler4Spec, fail)
+{
+    FAIL();
+}
