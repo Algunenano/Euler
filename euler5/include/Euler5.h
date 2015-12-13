841ef52deb11ecacbe2985a37c0b3ada6d72f453
Algunenano <algunenano@gmail.com>
2015-12-13 20:00:30 +0100

    Basic proyect for Problem 5

---
+++ euler5/include/Euler5.h
@@ -0,0 +1,5 @@
+#ifndef EULER5_H
+#define EULER5_H
+
+
+#endif
---
+++ euler5/test/Euler5Spec.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "Euler5.h"
+
+class Euler5Spec : public testing::Test
+{
+protected:
+
+    void SetUp()
+    {
+        
+    }
+
+    void TearDown()
+    {
+        // Teardown ...
+    }
+
+};
+
+TEST_F(Euler5Spec, fail)
+{
+    FAIL();
+}
