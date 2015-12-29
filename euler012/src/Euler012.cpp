a55a663ff2c7577919c94a71b0dac05a072788c9
Algunenano <algunenano@gmail.com>
2015-12-29 19:43:36 +0100

    Problem 12: Optimized and ugly

--- euler012/src/Euler012.cpp
+++ euler012/src/Euler012.cpp
@@ -6,29 +6,37 @@ ull triangleDivisors (ull _divisors)
     if (_divisors == 0) return 0;
     if (_divisors == 1) return 1;
 
-    std::vector<ull> primesV;
-    primesV.push_back(2);
+    std::vector<ull> primesV (1,2);
+    ull vSize = 1;
+    ull* vData = primesV.data();
+    
     ull num = 3;
     for (ull i = 3; ; num += i, i++)
     {
         //Check divisors of num
         ull check = num;
         ull div = 1;
-        for (ull d : primesV)
+        ull divisor = vData[0];
+        for (ull d = 0; (divisor <= check) && (d < vSize); d++, divisor = vData[d])
         {
-            ull factor = 0;
-            while (check % d == 0)
+            if (check % divisor == 0)
             {
-                factor++;
-                check /= d;
+                ull factor = 0;
+                while (check % divisor == 0)
+                {
+                    factor++;
+                    check /= divisor;
+                }
+                div *= factor + 1;
+                if (div >= _divisors) return num;
             }
-            div *= factor + 1;
-            if (div >= _divisors) return num;
         }
         
         if (check != 1)
         {
             primesV.push_back(check);
+            vSize++;
+            vData = primesV.data();
         }
     }
     
